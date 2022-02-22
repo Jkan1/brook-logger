62388e15ba03ac81e8cb632b1f4bc4b187cf89f0
JKan1 <kan.joshi.1@gmail.com>
2022-02-22 17:25:59 +0530

    Updated Tests

--- src/__tests__/createdLogs.js
+++ src/__tests__/createdLogs.js
@@ -1,14 +1,16 @@
-test("Check If Log Files Created", () => {
-  const fs = require("fs");
-  const brook = require("../../lib");
+test('Check If Log Files Created', () => {
+  const fs = require('fs');
+  const brook = require('../../lib');
   const logger = new brook.Logger({ preserveLogs: true });
 
-  logger.log("Logging Some message", [1, 2, 3, 4, 5]);
-  logger.warn("Warning This is an Object", { name: "kan" });
-  logger.error("Error Occurred while Executing", [1, 2, 3, 4, 5], JSON.stringify({ name: "kan" }));
+  logger.log('Logging Some message', [1, 2, 3, 4, 5]);
+  logger.warn('Warning This is an Object', { name: 'kan' });
+  logger.error('Error Occurred while Executing', [1, 2, 3, 4, 5], JSON.stringify({ name: 'kan' }));
 
-  const logFileName = logger.LOG_PATH + "logs-" + new Date().toISOString().slice(0, 10);
-  const errorLogFileName = logger.ERROR_LOG_PATH + "error-logs-" + new Date().toISOString().slice(0, 10);
+  const logFileName = logger.LOG_PATH + 'logs-' + new Date().toISOString().slice(0, 10);
+  const errorLogFileName = logger.ERROR_LOG_PATH + 'error-logs-' + new Date().toISOString().slice(0, 10);
 
-  expect(fs.existsSync(logFileName) && fs.existsSync(errorLogFileName)).toBe(true);
+  setTimeout(() => {
+    expect(fs.existsSync(logFileName) && fs.existsSync(errorLogFileName)).toBe(true);
+  }, 1000);
 });
