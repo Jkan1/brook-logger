ab21b26944969a0432744965c67db0f8ccf2cd15
JKan1 <kan.joshi.1@gmail.com>
2022-02-22 16:50:06 +0530

    Removed Initial Module Export Support

--- src/Logger.ts
+++ src/Logger.ts
@@ -2,7 +2,8 @@ import { createWriteStream, existsSync, mkdirSync } from 'fs';
 import Rotation from './Rotation';
 import Level from './Level';
 
-interface LoggerOptions {
+export interface LoggerOptions {
+  preserveLogs?: boolean;
   path?: string;
   errorPath?: string;
   rotation?: Rotation;
@@ -22,29 +23,30 @@ export default class Logger {
   private stdErrorStream: any;
 
   constructor(loggerOptions?: LoggerOptions) {
-    if (loggerOptions?.path) this.LOG_PATH = loggerOptions.path;
-    if (loggerOptions?.errorPath) this.ERROR_LOG_PATH = loggerOptions.errorPath;
-    if (loggerOptions?.rotation) this.LOG_ROTATION = loggerOptions.rotation;
+    if (loggerOptions?.preserveLogs) {
+      if (loggerOptions?.path) this.LOG_PATH = loggerOptions.path;
+      if (loggerOptions?.errorPath) this.ERROR_LOG_PATH = loggerOptions.errorPath;
+      if (loggerOptions?.rotation) this.LOG_ROTATION = loggerOptions.rotation;
 
-    if (this.LOG_ROTATION == Rotation.HOURLY) this.LOG_STAMP = new Date().toISOString().slice(0, 13);
+      if (this.LOG_ROTATION == Rotation.HOURLY) this.LOG_STAMP = new Date().toISOString().slice(0, 13);
 
-    if (!existsSync(this.LOG_PATH))
-      mkdirSync(this.LOG_PATH);
+      if (!existsSync(this.LOG_PATH))
+        mkdirSync(this.LOG_PATH);
 
-    if (this.ERROR_LOG_PATH && !existsSync(this.ERROR_LOG_PATH))
-      mkdirSync(this.ERROR_LOG_PATH);
+      if (this.ERROR_LOG_PATH && !existsSync(this.ERROR_LOG_PATH))
+        mkdirSync(this.ERROR_LOG_PATH);
 
-    this.stdOutName = this.LOG_PATH + 'logs-' + this.LOG_STAMP;
-    this.stdErrorName = (this.ERROR_LOG_PATH || this.LOG_PATH) + 'error-logs-' + this.LOG_STAMP;
+      this.stdOutName = this.LOG_PATH + 'logs-' + this.LOG_STAMP;
+      this.stdErrorName = (this.ERROR_LOG_PATH || this.LOG_PATH) + 'error-logs-' + this.LOG_STAMP;
 
-    if (loggerOptions?.logFileExtension) {
-      this.stdOutName += '.log';
-      this.stdErrorName += '.log';
+      if (loggerOptions?.logFileExtension) {
+        this.stdOutName += '.log';
+        this.stdErrorName += '.log';
+      }
+
+      this.stdOutStream = createWriteStream(this.stdOutName, { flags: 'a' });
+      this.stdErrorStream = createWriteStream(this.stdErrorName, { flags: 'a' });
     }
-
-    this.stdOutStream = createWriteStream(this.stdOutName, { flags: 'a' });
-    this.stdErrorStream = createWriteStream(this.stdErrorName, { flags: 'a' });
-
     if (loggerOptions?.initLog)
       this.log('Logger Initialized');
   }
@@ -81,18 +83,24 @@ export default class Logger {
 
   public log(...data: any) {
     const timestamp = new Date().toISOString();
-    this.stdOutLog(timestamp, '- ' + Level.INFO + ' ~', ...data);
+    if (this.stdOutStream)
+      this.stdOutLog(timestamp, '- ' + Level.INFO + ' ~', ...data);
     console.log(timestamp, '- ' + Level.INFO + ' ~', ...data);
   }
+
   public warn(...data: any) {
     const timestamp = new Date().toISOString();
-    this.stdOutLog(timestamp, '- ' + Level.WARN + ' ~', ...data);
+    if (this.stdOutStream)
+      this.stdOutLog(timestamp, '- ' + Level.WARN + ' ~', ...data);
     console.warn(timestamp, '- ' + Level.WARN + ' ~', ...data);
   }
+
   public error(...data: any) {
     const timestamp = new Date().toISOString();
-    this.stdErrorLog(timestamp, '- ' + Level.ERROR + ' ~', ...data);
-    this.stdOutLog(timestamp, '- ' + Level.ERROR + ' ~', ...data);
+    if (this.stdErrorStream)
+      this.stdErrorLog(timestamp, '- ' + Level.ERROR + ' ~', ...data);
+    if (this.stdOutStream)
+      this.stdOutLog(timestamp, '- ' + Level.ERROR + ' ~', ...data);
     console.error(timestamp, '- ' + Level.ERROR + ' ~', ...data);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import Logger from './Logger';
+/**
+ *  
+ * Welcome to Brook-Logger
+ * 
+ */
 
-export default Logger;
-
-module.exports = Logger;
+export { default as Logger } from './Logger';
 
 export { default as Rotation } from './Rotation';
+
 export { default as Level } from './Level';
